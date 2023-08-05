c9a684c3c3f766113b6d8c208b5e9e7de610ab88
VictorNThuo <141309174+VictorNThuo@users.noreply.github.com>
2023-08-05 05:19:39 +0300

    Create else if.R

---
+++ Learning R/else if.R
@@ -0,0 +1,17 @@
+x <- 1
+
+# run the code
+
+if (x < 0) {
+  
+  print("x is a negative number")
+  
+} else if (x == 0) {
+  
+  print("x is zero")
+  
+} else {
+  
+  print("x is a positive number")
+  
+}
